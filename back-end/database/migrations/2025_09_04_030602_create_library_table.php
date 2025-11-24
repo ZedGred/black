7e6a0815245b467cad5f92db018eea7c1672a78f
biyan <pebiyanfirmansyah22@gmail.com>
2025-11-24 16:57:58 +0700

    Add backend

--- back-end/database/migrations/2025_09_04_030602_create_library_table.php
+++ back-end/database/migrations/2025_09_04_030602_create_library_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('bookmarks', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('article_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('user_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('article_id')->constrained()->onDelete('cascade');
             $table->timestamps();
             $table->unique(['user_id', 'article_id']); // biar gak dobel
+            $table->primary(['user_id', 'article_id']); 
         });
     }
 
